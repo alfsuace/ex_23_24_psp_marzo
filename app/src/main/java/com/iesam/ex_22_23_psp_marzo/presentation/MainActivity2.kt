99400be9ea38b64663a53d8659278f3d55b5a8fd
alfsuace <alfonso.suaace@educa.jcyl.es>
2024-05-31 16:38:29 +0200

    ex/2:
    running until app is closed

--- app/src/main/java/com/iesam/ex_22_23_psp_marzo/presentation/MainActivity2.kt
+++ app/src/main/java/com/iesam/ex_22_23_psp_marzo/presentation/MainActivity2.kt
@@ -1,9 +1,13 @@
 package com.iesam.ex_22_23_psp_marzo.presentation
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.iesam.ex_22_23_psp_marzo.R
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,6 +24,12 @@ class MainActivity2 : AppCompatActivity() {
     }
 
     private fun initExercise2() {
+        GlobalScope.launch {
+            while (true){
+                delay(2000)
+                Log.d("ex2", "running")
+            }
+        }
 
     }
 }
